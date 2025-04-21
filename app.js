b187a4bd0fd4c44ce60d74e4b77878a6adef0369
Gokulrajan <143718403+ngrajan@users.noreply.github.com>
2025-04-21 23:49:10 +0530

    error handling logic

--- app.js
+++ app.js
@@ -20,6 +20,21 @@ app.use('/api/v1/users', userRouter);
 //     status: 'fail',
 //     message: `The page ${req.originalUrl} is not found`,
 //   });
+
+//   const err = new Error(`Can't find ${req.originalUrl}on this server`);
+//   err.statusCode = 404;
+//   err.status = 'fail';
+
+//   next(err);
 // });
 
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 module.exports = app;
