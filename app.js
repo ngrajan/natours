6265e0341fe58e541e85c5179f501a0bf6b4eb47
Gokulrajan <143718403+ngrajan@users.noreply.github.com>
2025-04-22 23:17:24 +0530

    working on http all method --> app.all()

--- app.js
+++ app.js
@@ -15,18 +15,18 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 // !Bug
-// app.all('*', (req, res, next) => {
-//   res.status(404).json({
-//     status: 'fail',
-//     message: `The page ${req.originalUrl} is not found`,
-//   });
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `The page ${req.originalUrl} is not found`,
+  });
 
-//   const err = new Error(`Can't find ${req.originalUrl}on this server`);
-//   err.statusCode = 404;
-//   err.status = 'fail';
+  const err = new Error(`Can't find ${req.originalUrl}on this server`);
+  err.statusCode = 404;
+  err.status = 'fail';
 
-//   next(err);
-// });
+  next(err);
+});
 
 app.use((err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
