ced7c70b25cb98299ddb2aa49c7af705ff0003de
Gokulrajan <143718403+ngrajan@users.noreply.github.com>
2025-04-30 23:59:40 +0530

    router update

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
 
-const router = express.Router();
+// const router = express.Router();
 
-router
-  .route('/top-5-tours')
-  .get(tourController.aliasTopTours, tourController.getAllTours);
+// router
+//   .route('/top-5-tours')
+//   .get(tourController.aliasTopTours, tourController.getAllTours);
 
-router.route('/tour-stats').get(tourController.getTourStats);
+// router.route('/tour-stats').get(tourController.getTourStats);
 
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+// router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
-router
-  .route('/')
-  .get(tourController.getAllTours)
-  .post(tourController.createTour);
-router
-  .route('/:id')
-  .get(tourController.getTour)
-  .patch(tourController.updateTour)
-  .delete(tourController.deleteTour);
+// router
+//   .route('/')
+//   .get(tourController.getAllTours)
+//   .post(tourController.createTour);
+// router
+//   .route('/:id')
+//   .get(tourController.getTour)
+//   .patch(tourController.updateTour)
+//   .delete(tourController.deleteTour);
 
-module.exports = router;
+// module.exports = router;
