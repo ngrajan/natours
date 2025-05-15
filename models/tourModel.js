768e682e6b4e57473f3b089b813afe411334e148
Gokulrajan <143718403+ngrajan@users.noreply.github.com>
2025-05-16 00:20:03 +0530

    implemented authentication with jwt and managing encrypted password

--- models/tourModel.js
+++ models/tourModel.js
@@ -126,7 +126,6 @@ tourSchema.pre(/^find/, function (next) {
 // Aggregation Middleware
 tourSchema.pre('aggregate', function (next) {
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-  console.log(this);
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
