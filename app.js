d69d1aad6055a19c37471fa4a9c6172d9f60bf60
Gokulrajan <143718403+ngrajan@users.noreply.github.com>
2025-10-06 22:15:37 +0530

    integrated mapbox settings

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.set('views', path.join(__dirname, 'views'));
 // app.use(express.static(`${__dirname}/public`));
 app.use(express.static(path.join(__dirname, 'public')));
 // setting security http
-app.use(helmet());
+// ! security issue on content security policy on mapbox.
+// app.use(helmet());
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
--- public/js/mapBox.js
+++ public/js/mapBox.js
@@ -1,1 +1,11 @@
-console.log('hello from client side');
+/* eslint-disable */
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoia2stNDUiLCJhIjoiY21nZXVvbTY4MDFoazJrcDk3dmlnZ3NteiJ9.z0xbYPq4O1sKnPyAqYvlBw';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/kk-45/cmgexo5uh000601sd8e6gcs9l',
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 10,
+});
