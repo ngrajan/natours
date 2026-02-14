4606a61f57b694bdd2806b466f8a2c2e868f7860
Gokulrajan <143718403+ngrajan@users.noreply.github.com>
2026-02-14 22:51:30 +0530

    working on emailing functionality

--- utils/email.js
+++ utils/email.js
@@ -1,15 +1,40 @@
 const nodemailer = require('nodemailer');
 
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Gokulrajan <${process.env.EMAIL_FROM}>`;
+  }
+
+  createTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // use sendgrid
+      return 1;
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  send(template, subject) {
+    return 1;
+  }
+
+  sendWelcome() {
+    this.send();
+  }
+};
+
 const sendEmail = async (options) => {
   // 1) create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
 
   // 2) Create email options
   const mailOptions = {
